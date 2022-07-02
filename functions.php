0aba7483f71ca72f1fccad37db370b0cc87dce44
arrancurran <arrancurran@gmail.com>
2022-07-02 16:14:07 +0200

    styled post nav

--- functions.php
+++ functions.php
@@ -417,3 +417,66 @@ function slow_atoms_get_random_hero( $wrapper_class , $image_class ) {
 return;
 
 }
+
+
+// Using Thumbnails with Previous and Next Post Links
+
+function slow_atoms_posts_nav() {
+	$next_post = get_next_post() ;
+  $prev_post = get_previous_post() ;
+
+  if ( $next_post || $prev_post ) : ?>
+
+	<div class="slow-atoms-posts-nav">
+		<h2 class="screen-reader-text">Post navigation</h2>
+		<div> <?php
+
+			if ( ! empty( $prev_post ) ) : ?>
+
+			<a href="<?php echo get_permalink( $prev_post ); ?>">
+				<div>
+					<div class="slow-atoms-posts-nav__thumbnail slow-atoms-posts-nav__prev">
+						<?php echo get_the_post_thumbnail( $prev_post, [ 100, 100 ] ); ?>
+					</div>
+				</div>
+
+				<div>
+					<strong>
+						<i class="fas fa-arrow-circle-left"></i>
+
+						<?php _e( 'Previous project', 'textdomain' ) ?>
+          </strong>
+
+					<h4><?php echo get_the_title( $prev_post ); ?></h4>
+				</div>
+      </a> <?php
+
+			endif; ?>
+    </div>
+
+    <div> <?php
+
+		if ( ! empty( $next_post ) ) : ?>
+
+		<a href="<?php echo get_permalink( $next_post ); ?>">
+			<div>
+				<strong>
+          <?php _e( 'Next project', 'textdomain' ) ?>
+          <i class="fas fa-arrow-circle-right"></i>
+        </strong>
+
+				<h4><?php echo get_the_title( $next_post ); ?></h4>
+      </div>
+
+			<div>
+        <div class="slow-atoms-posts-nav__thumbnail slow-atoms-posts-nav__next">
+          <?php echo get_the_post_thumbnail( $next_post, [ 100, 100 ] ); ?>
+        </div>
+      </div>
+    </a> <?php
+
+		endif; ?>
+  </div>
+</div> <!-- .slow-atoms-posts-nav --> <?php
+
+endif ; }
