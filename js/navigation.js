b53fcdad4a6e394e78ee670173b107cbc212352a
arrancurran <arrancurran@gmail.com>
2022-11-15 18:37:19 +0100

    snappier ani and medi query to stop on desktop

--- js/navigation.js
+++ js/navigation.js
@@ -5,32 +5,36 @@
  * navigation support for dropdown menus.
  */
 
+const mediaQuery = window.matchMedia('(max-width: 768px)')
 
-const navSlide = ()=>{
-  const burger = document.querySelector('.burger');
-  const navSlider = document.querySelector('.menu-navbar-container');
-  const navLinks = document.querySelectorAll('.navbar--list > li');
-  const navBar = document.querySelector('.main-navigation');
+if (mediaQuery.matches) {
+  
+  const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const navSlider = document.querySelector('.menu-navbar-container');
+    const navLinks = document.querySelectorAll('.navbar--list > li');
+    const navBar = document.querySelector('.main-navigation');
 
-  // Return early if the navigation doesn't exist.
-	if ( ! navLinks ) {
-		return;
-	}
+    // Return early if the navigation doesn't exist.
+    if ( ! navLinks ) {
+      return;
+    }
 
-  // watch for touch clicks on the whole navbar
-  navBar.addEventListener('click', () => {
-    // animate accordinaly
-    navSlider.classList.toggle('nav-active');
-    burger.classList.toggle('burger-active');
-    navLinks.forEach((link, index) => {
-      if ( link.style.animation ) {
-        link.style.animation = ``
-      } else {
-        link.style.animation = `navLinksFade .4s ease-out forwards ${index * .1 + .1 }s`
-      }
+    // watch for touch clicks on the whole navbar
+    navBar.addEventListener('click', () => {
+      // animate accordinaly
+      navSlider.classList.toggle('nav-active');
+      burger.classList.toggle('burger-active');
+      navLinks.forEach((link, index) => {
+        if ( link.style.animation ) {
+          link.style.animation = ``
+        } else {
+          link.style.animation = `navLinksFade .25s ease-out forwards ${index * .1  }s`
+        }
+      });
+      burger.classList.toggle('toggle');
     });
-    burger.classList.toggle('toggle');
-  });
+  }
+  navSlide();
 }
 
-navSlide();
