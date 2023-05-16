c036c60b1f8f3e8e1bda03c8c77fa9f5d2d390f8
arrancurran <arrancurran@gmail.com>
2023-05-16 15:09:47 +0200

    more colors

--- inc/functions/callbacks/customise-colors.php
+++ inc/functions/callbacks/customise-colors.php
@@ -26,15 +26,16 @@ function slow_atoms_customise_colors() {
 
 	$slow_atoms_theme_color_darker = colourBrightness( $slow_atoms_primary_theme_color, -.3);
 
-	$alphas = array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9) ;
+	$alphas = array(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95) ;
 
-	$css_root_colors_alphas = ''; $i = 1 ;
+	$css_root_colors_alphas = '';
 
 	foreach ( $alphas as $alpha ) {
 		$primary_alpha		= hex2rgba( $slow_atoms_primary_theme_color		, $alpha ) ;
 		$secondary_alpha	= hex2rgba( $slow_atoms_secondary_theme_color	, $alpha ) ;
-		$css_root_colors_alphas .= '--theme-color-primary-alpha-0' 		. $i . '0 : ' . $primary_alpha . ' ;' . "\r\n";
-		$css_root_colors_alphas .= '--theme-color-secondary-alpha-0' 	. $i . '0 : ' . $secondary_alpha . ' ;' . "\r\n";
+		$alpha_str			= str_replace('.', '',sprintf("%' .2f", $alpha) );
+		$css_root_colors_alphas .= '--theme-color-primary-alpha-' 		. $alpha_str . ' : ' . $primary_alpha . ' ;' . "\r\n";
+		$css_root_colors_alphas .= '--theme-color-secondary-alpha-' 	. $alpha_str . ' : ' . $secondary_alpha . ' ;' . "\r\n";
 		$i ++ ;
 	}
 
