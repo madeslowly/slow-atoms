d930534b0462083894fb8de44b7d8b8ee211b063
arrancurran <arrancurran@gmail.com>
2023-05-30 14:36:27 +0200

    img markup

--- template-parts/content-ms_equipment.php
+++ template-parts/content-ms_equipment.php
@@ -9,26 +9,31 @@
 
 
 $ms_acf_equip_images =  get_field('ms_acf_equip_images') ;
+$ms_acf_equip_desc	 =  get_field('ms_acf_equip_description_key') ;
 
 for ( $i = 1 ; $i <= 4 ; $i++ ) {
 
 	$image = 'image_' . $i ;
 
 	if ( empty( $ms_acf_equip_images[ $image ] ) ) :
-		// if we have reached the last author, break
+		// if we have reached the last image, break
 		break ;
 	else :
+		$image_URL		= $ms_acf_equip_images[ $image ] ;
+		$image_ID		= attachment_url_to_postid( $image_URL ) ;
+		$image_srcset	= wp_get_attachment_image_srcset( $image_ID , 'full' );
 
-		$html_out .= '<img src="' . $ms_acf_equip_images[ $image ] . '" />' ;
-
-		endif ;
+		$image_markup  .= 
+		'<div class="' . $wrapper_class . '">
+			<img class="' . $image_class . '" loading="lazy" src="' . $image_URL . '" srcset="' . esc_attr( $image_srcset ) . '" />
+		</div><!-- .' . $wrapper_class . '-->' ;
+		
+	endif ;
 
 }
 
-
 $post_id = $post -> ID ;
 $service_post = $post ;
-
 
 if ( is_singular() ) :
 
@@ -58,7 +63,9 @@ endif ;
 
 		the_title( '<h2 class="contact-form__header">', '</h2>' ) ;
 
-		echo $html_out ;
+		echo $image_markup ;
+
+		echo '<p>' . $ms_acf_equip_desc . '</p>';
 
 		if ( is_user_logged_in( ) ) {
 			slow_atoms_equipment_booking_form( $service_post, wp_get_current_user(), ''  ) ;
