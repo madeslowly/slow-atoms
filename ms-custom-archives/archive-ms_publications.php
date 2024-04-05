df82acd59f3a4a431ec9dc6221f9289c6a37586a
arrancurran <arrancurran@gmail.com>
2024-04-05 15:39:05 +0200

    format

--- ms-custom-archives/archive-ms_publications.php
+++ ms-custom-archives/archive-ms_publications.php
@@ -11,65 +11,66 @@ get_header();
 
 ?>
 
-	<main id="primary" class="site-main is__publication-archive">
+<main id="primary" class="site-main is__publication-archive">
 
-		<section class="slow-atoms__page-hero">
+	<section class="slow-atoms__page-hero">
 
-			<header class="page-header is__theme-background-transparent"> <?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );	?>
-			</header><!-- .page-header -->
-			
-			<?php slow_atoms_get_random_hero('post-thumbnail' ,'attachment-post-thumbnail size-post-thumbnail wp-post-image'); ?>
+		<header class="page-header is__theme-background-transparent"> 
+			<?php the_archive_title( '<h1 class="page-title">', '</h1>' ) ; ?>
+		</header><!-- .page-header -->
+		
+		<?php slow_atoms_get_random_hero('post-thumbnail' ,'attachment-post-thumbnail size-post-thumbnail wp-post-image'); ?>
 
-		</section><!-- .slow-atoms__page-hero -->
+	</section><!-- .slow-atoms__page-hero -->
 
-		<section class="entry-content">
-			<?php
+	<section class="entry-content">
 
-				$years = array();
-				$year = '' ;
+<?php
 
-				if ( have_posts() ) :
-				/* Start the Loop */
-				while ( have_posts() ) :
-					
-					the_post();
+		$years = array();
+		$year = '' ;
 
-					$previous_year = $year ;
-					$year = get_the_date( 'Y' ) ;
+		if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) :
+				
+				the_post();
 
-					if ( $year != $previous_year ) :
-						echo '<h2 class="publication__year-title">' .  $year . '</h2>' ;
-					endif ;
+				$previous_year = $year ;
+				$year = get_the_date( 'Y' ) ;
 
-					if ( ! isset( $years[ $year ] ) ) 
-						$years[ $year ] = array();
-      					$years[ $year ][] = array( 'title' => get_the_title(), 'permalink' => get_the_permalink() );
+				if ( $year != $previous_year ) :
+					echo '<h2 class="publication__year-title">' .  $year . '</h2>' ;
+				endif ;
 
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_type() );
+				if ( ! isset( $years[ $year ] ) ) 
+					$years[ $year ] = array();
+					$years[ $year ][] = array( 'title' => get_the_title(), 'permalink' => get_the_permalink() );
+
+				/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+				get_template_part( 'template-parts/content', get_post_type() );
 
 
-				endwhile;
+			endwhile;
 
-				the_posts_navigation( array(
-					'prev_text'	=> 'Older Publications',
-					'next_text' => 'Newer Publications',
-					'class'			=> 'slow-atoms-posts-nav'
-				));
+			the_posts_navigation( array(
+				'prev_text'	=> 'Older Publications',
+				'next_text' => 'Newer Publications',
+				'class'			=> 'slow-atoms-posts-nav'
+			));
 
-			else :
+		else :
 
-				get_template_part( 'template-parts/content', 'none' );
+			get_template_part( 'template-parts/content', 'none' );
 
-			endif;
-			?>
-		</section>
-	</main><!-- #main -->
+		endif;
+?>
+	</section>
+</main><!-- #main -->
 
 <?php
 
