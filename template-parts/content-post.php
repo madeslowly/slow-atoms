06106d42d40cc0868e60fb4a1c3226ca5b13e90e
arrancurran <arrancurran@gmail.com>
2024-04-16 11:15:06 +0200

    removed header

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -9,64 +9,62 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('is__default-content-page'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('is__news-content-entry'); ?> >
 
-
-
-    <section class="slow-atoms__page-hero">
-        <header class="page-header is__theme-background-transparent">
-			<?php the_title( '<h2 class="page-title">', '</h2>' );?>
-
-        </header><!-- .page-header .is__theme-background-transparent -->
-
-		<div class="post-thumbnail">
-        <?php 
+	<div class="news-thumbnail">
+		<?php 
 		
-		if ( has_post_thumbnail() ) :
+			if ( has_post_thumbnail() ) :
+				
+				the_post_thumbnail();
+
+			else :
 			
-			the_post_thumbnail();
-
-		else :
-		
-			slow_atoms_get_random_hero('post-thumbnail' , 'attachment-post-thumbnail size-post-thumbnail wp-post-image'); 
-			
-		endif ;
+				slow_atoms_get_random_hero('post-thumbnail' , 'attachment-post-thumbnail size-post-thumbnail wp-post-image'); 
+				
+			endif ;
 		?>
-		</div>
-    </section>
+	</div>
+	
+	<div class="news-body">
+		<header class="news-header">
+			<?php the_title( '<h2 class="news-title">', '</h2>' ) ; ?>
+		</header><!--  -->
 
-	<div class="entry-content">
-		<?php
-		the_content();
+		<div class="entry-content">
+			<?php
+			the_content();
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'slow-atoms' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'slow-atoms' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+		</div><!-- .entry-content -->
+		<!-- <div class="blur"></div> -->
+	</div><!-- .news-body -->
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'slow-atoms' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			// edit_post_link(
+			// 	sprintf(
+			// 		wp_kses(
+			// 			/* translators: %s: Name of current post. Only visible to screen readers */
+			// 			__( 'Edit <span class="screen-reader-text">%s</span>', 'slow-atoms' ),
+			// 			array(
+			// 				'span' => array(
+			// 					'class' => array(),
+			// 				),
+			// 			)
+			// 		),
+			// 		wp_kses_post( get_the_title() )
+			// 	),
+			// 	'<span class="edit-link">',
+			// 	'</span>'
+			// );
 			?>
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
