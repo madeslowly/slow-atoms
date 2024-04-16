24dffdd83194a446ee5f04683672966576e12c9f
arrancurran <arrancurran@gmail.com>
2024-04-16 11:13:06 +0200

    removed is_home conditional

--- index.php
+++ index.php
@@ -15,20 +15,13 @@
  */
 
 get_header();
+
 ?>
 
 	<main id="primary" class="site-main index">
 
 		<?php
 		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
 
 			/* Start the Loop */
 			while ( have_posts() ) :
