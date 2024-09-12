4c3f91295e4abad7846a15b36bc8c7492296f90d
arrancurran <arrancurran@gmail.com>
2024-09-12 22:55:45 +0200

    resuced sqls and added id seperation

--- inc/functions/callbacks/equipment-booking-form.php
+++ inc/functions/callbacks/equipment-booking-form.php
@@ -14,110 +14,117 @@
 
  function slow_atoms_equipment_booking_form( $service_post, $current_user ) {
 
+	global $wpdb ;
+
 	$booking_period	= 84 ; // 3 months
-	
+	// Get service id and name based on post id
 	$service_id		= $service_post -> ID ;
 	$service_name	= $service_post -> post_title ;
-
-	global $wpdb ;
-	
-	$all_bookings_sql = $wpdb -> prepare(
-		"SELECT booked_date, email 
-		FROM sa_booked_dates 
-		JOIN sa_bookings ON sa_booked_dates.booking_id = sa_bookings.booking_id
-		WHERE sa_bookings.service_id = $service_id");
-	$all_bookings = $wpdb -> get_results( $all_bookings_sql ) ;
-	
 	// Get user email and name
 	$current_user_email = $current_user -> user_email ;
-	$current_user_name  = get_user_meta( $current_user -> ID, 'first_name', true ) ;
+	$current_user_name = $current_user ? $current_user->first_name . ' ' . $current_user->last_name : '';
 
-	// Fallback if user hasn't set first_name
+	// Fallback if user hasn't set first_name and last_name
 	if ( ! $current_user_name ) { 
 		$current_user_name = $current_user -> user_login ; 
 	}
-
+	// Get current time and time at midnight
 	$current_datetime   = current_datetime();
 	$current_time       = $current_datetime -> getTimestamp() ;
 	$time_at_midnight   = $current_time - ( $current_time % 86400 ) - $current_datetime -> getOffset() ;
 	
-	// Get booked dates
-	$query_booked       = $wpdb -> prepare( "SELECT booked_date FROM sa_booked_dates" ) ;
-	$sa_booked_dates    = $wpdb -> get_col( $query_booked ) ; // Array of booked times in unix format
-	
 	/**
-	 * 
-	 * Check for old bookings and cleanup db
-	 * 
+	 * sa_booked_dates table Queries
 	 */
+	
+	// Get all existing bookings for this service_id from both tables
+	$sql_query = $wpdb -> prepare(
+		"SELECT sa_booked_dates.*, sa_bookings.email
+		FROM sa_booked_dates
+		INNER JOIN sa_bookings 
+		ON sa_booked_dates.booking_id = sa_bookings.booking_id
+		WHERE sa_bookings.service_id = %d", $service_id );
 
-	// Check if date is in the past and delete that db entry if it is
-	foreach ( $sa_booked_dates as $booked_date ) {
-		if ( $time_at_midnight > $booked_date ) {
-			$wpdb -> delete( 'sa_booked_dates', array( 'booked_date' => $booked_date ) );
-			echo $booked_date ;
-		}
+	$sa_booked_dates = $wpdb -> get_results( $sql_query ) ;
+
+	$sql_query  = $wpdb -> prepare( 
+		"SELECT *
+		FROM sa_bookings
+		WHERE `service_id` = %d", $service_id) ;
+
+	$sa_bookings = $wpdb -> get_results( $sql_query ) ;
+
+	// Check for booked dates that are in the past and delete them
+	foreach ( $sa_booked_dates as $booking ) {
+		if ( $time_at_midnight > $booking -> booked_date ) {
+			$wpdb->delete( 'sa_booked_dates', array( 'booked_date' => $booking -> booked_date, 'service_id' => $service_id ) );
+	} }
+
+	// For each booking check if there are still dates in the sa_booked_dates table
+	foreach( $sa_bookings as $booking ) {
+		$booking_id_exists = false ;
+		foreach ( $sa_booked_dates as $date ) {
+			if ( $date -> booking_id === $booking -> booking_id ) {
+				$booking_id_exists = true ;
+				// The booking has a date so break
+				break ;
+		} }
+		// Delete the booking if it has no dates
+		if ( ! $booking_id_exists ) {
+			$wpdb -> delete( 'sa_bookings', array( 'booking_id' => $booking -> booking_id ) );
+		} }
+	
+	// Format current booked dates for flatpickr. Array -> string "time", "time2" etc
+	$flatpickr_booked_dates = "";
+	foreach ( $sa_booked_dates as $booking ) {
+		$flatpickr_booked_dates .= '"' . $booking -> booked_date . '", ';
 	}
-
-	$query_booked_id    = $wpdb -> prepare( "SELECT booking_id FROM sa_booked_dates" ) ;
-	$sa_booked_dates_id	= $wpdb -> get_col( $query_booked_id ) ;
-
-	$query_bookings_id  = $wpdb -> prepare( "SELECT booking_id FROM sa_bookings" ) ;
-	$sa_bookings_id		= $wpdb -> get_col( $query_bookings_id ) ;
-
-	foreach( $sa_bookings_id as $id ) {
-
-		$booking_id_exits = array_search( $id, $sa_booked_dates_id ) ;
-
-		if ( $booking_id_exits === false ) {
-			$wpdb -> delete( 'sa_bookings', array( 'booking_id' => $id ) );
-		}
-		
-	}
-
-	// Format for flatpickr. Array -> string "time", "time2" etc
-	$sa_booked_dates        = '"' . implode ( '", "', $sa_booked_dates ) . '"' ;
-
-	// Get all bookings from current user
-	$dbquery            = $wpdb -> prepare( "SELECT * FROM `sa_bookings` WHERE `email` = %s ", $current_user_email ) ;
-	$sabk_user_bk_ids   = $wpdb -> get_col( $dbquery ) ;
+	$flatpickr_booked_dates = rtrim($flatpickr_booked_dates, ', ');
+	
+	// Get all booking IDs for the current user
+	$users_bookings = array();
+	foreach ($sa_bookings as $booking) {
+		if ($booking->email === $current_user_email) {
+			$users_bookings[] = $booking->booking_id;
+	} }
 	
 	?>
 	
+	<h2>Bookings for <?php echo $service_name ; ?></h2>
+
+	<?php
+	if ( ! empty( $sa_booked_dates ) ) {
+		$markup = '<h4>Booked Dates</h4>' ;
+		$markup .= '<div class="table-container">' ;
+		$markup .= '<table><tr><th>Date</th><th>User</th></tr>';
+				
+		// Sort the result array by booked_date in ascending order
+		usort($sa_booked_dates, function($a, $b) {
+			return $a->booked_date - $b->booked_date;
+		});
+
+		// Output data of each row
+		foreach ( $sa_booked_dates as $row ) {
+			$date		= date("D d M", esc_html( $row -> booked_date + ( get_option( 'gmt_offset' ) * 3600 ) ) ) ;
+			$email		= esc_html( $row -> email ) ;
+			$user		= get_user_by( 'email', $email ) ;
+			$full_name	= $user ? $user->first_name . ' ' . $user->last_name : '';
+			
+			$markup .= "<tr><td>$date</td><td><a href='mailto:$email?subject=Your $service_name Booking - $date'>$full_name</a></td></tr>";
+		}				
+		$markup .= '</table></div>' ;
+
+		echo $markup ;
+	}
+	?>
+
+	<!-- New Booking Form -->
 	<form class="sa__form" id="booking_form" name="form" method="POST" action="<?php echo admin_url( 'admin-post.php' ); ?>" >
 	
-		<h2>Bookings for <?php echo $service_name ; ?></h2>
-
-		<?php
-		if (!empty($all_bookings)) {
-		
-			$markup = '<h4>Booked Dates</h4>';
-			$markup .= '<div class="table-container">' ;
-			$markup .= '<table><tr><th>Date</th><th>User</th></tr>';
-
-			echo $markup ;
-					
-			// Sort the result array by booked_date in ascending order
-			usort($all_bookings, function($a, $b) {
-				return $a->booked_date - $b->booked_date;
-			});
-
-			// Output data of each row
-			foreach ($all_bookings as $row) {
-				$date = date("D d-M", esc_html($row->booked_date));
-				$email = esc_html($row->email);
-				$user = get_user_by('email', $email);
-				$full_name = $user ? $user->first_name . ' ' . $user->last_name : '';
-				echo "<tr><td>$date</td><td><a href='mailto:$email?subject=Your $service_name Booking - $date'>$full_name</a></td></tr>";
-			}
-					
-			echo '</table></div>' ;
-		}
-		?>
 		<h4>New Booking</h4>
 
 		<p>Hi <?php echo $current_user_name ?>, select the date(s) you want to book.</p>
-		<p class="small">Greyed out dates are already booked or fall outside of the maximum booking period (<?php echo $booking_period ; ?> days from today). If another user books your chosen date before you finish, you will be returned here with an updated list of available dates.</p>
+		<p class="small">Greyed out dates are booked or fall outside of the maximum booking period. If another user books your chosen date before you finish, you will be returned here with an updated list of available dates.</p>
 
 		<input type="hidden" name="action" value="process_booking" />
 
@@ -127,7 +134,7 @@
 
 		<input type="hidden" id="email" name="sabk-email" pattern=".+@ru\.nl" value="<?php echo $current_user_email ?>" required>
 
-		<input type="text" id="sabk-dates" name="sabk-dates" placeholder="Select dates">
+		<input type="text" id="sabk-dates" name="sabk-dates" placeholder="Select dates" required>
 
 		<input type="submit" value="Submit" class="slow-atoms__button">
 
@@ -143,28 +150,32 @@
 			altFormat: "D j F, Y",
 			dateFormat: "U",
 			theme: "default",
-			disable: [<?php echo $sa_booked_dates ; ?>],
-			// onChange: function( selectedDates, dateStr, instance ) {
-			//     console.log( dateStr ) ;
-			// },
+			disable: [<?php echo $flatpickr_booked_dates ; ?>],
 		});
+		// Check if dates are selected before submitting the form
+		document.getElementById('booking_form').addEventListener('submit', function(event) {
+			var datesField = document.getElementById('sabk-dates');
+			if (!datesField.value) {
+				event.preventDefault();
+			alert('Please select at least one date before submitting the form.');
+		}});
 	</script>
 
 	<?php
-	if ( $sabk_user_bk_ids ) {
+	if ( $users_bookings ) {
 		$current_bookings_title	= 'Your Current Bookings' ;
 		$current_bookings_inst	= 'Your bookings are listed below. You can select an existing booking and add or remove dates for that booking then click "Update" to complete the change.' ;
 		echo '<h4>' . $current_bookings_title . '</h4>' ;
 		echo '<p>' . $current_bookings_inst . '</p>' ;
-		foreach ( $sabk_user_bk_ids as $sabk_id ) {
+		foreach ( $users_bookings as $sabk_id ) {
 
 			// Get dates of current booking
-			$dbquery     = $wpdb -> prepare( "SELECT `booked_date` FROM `sa_booked_dates` WHERE `booking_id` = %d ", $sabk_id ) ;
+			$dbquery     = $wpdb -> prepare( "SELECT `booked_date` FROM `sa_booked_dates` WHERE `booking_id` = %d AND `service_id` = %d", $sabk_id, $service_id ) ;
 			$sabk_booked = $wpdb -> get_col( $dbquery ) ;
 			
 			$sabk_booked = '"' . implode ( '", "', $sabk_booked ) . '"' ;
 			
-			$sabk_bk_other = str_replace($sabk_booked , "",  $sa_booked_dates);
+			$sabk_bk_other = str_replace($sabk_booked , "",  $flatpickr_booked_dates);
 			?>
 			<div class="booking_accordion" value="<?php echo $sabk_id ?>" >
 				<p><?php echo $service_name . ' booking #' . $sabk_id ; ?></p>
@@ -195,9 +206,9 @@
 					defaultDate: [<?php echo $sabk_booked ?>]
 				});
 			</script>
-	<?php
+		<?php
 		}
-	?>
+		?>
 		<script>
 			var acc = document.getElementsByClassName( "booking_accordion" ) ;
 			var i ;
@@ -227,15 +238,12 @@
 	<?php
 	}
 
-	// $notif = $_GET['notif'] ;
-
+	// Notification popup
 	if (isset($_GET['notif']) && !empty($_GET['notif'])) {
 		$notif = $_GET['notif'] ;
 		$notif_popup = '
 		<input id="modal-toggle" type="checkbox">
-		
 		<div class="notif__screen">
-			
 			<div class="notif__wrap">
 				<h4 class="notif__text">' . $notif . '</h4>
 				<label class="modal-content-btn" for="modal-toggle">OK</label>
